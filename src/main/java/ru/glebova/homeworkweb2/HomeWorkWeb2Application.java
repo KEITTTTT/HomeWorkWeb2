25668fe81fd80d092f010b1b94db2c99478e5664
Glebova <e.tulzakova@mail.ru>
2023-01-22 17:07:53 +0400

    Initial commit

---
+++ src/main/java/ru/glebova/homeworkweb2/HomeWorkWeb2Application.java
@@ -0,0 +1,13 @@
+package ru.glebova.homeworkweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkWeb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkWeb2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/glebova/homeworkweb2/HomeWorkWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.glebova.homeworkweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkWeb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
